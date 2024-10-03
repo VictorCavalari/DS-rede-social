9761f925da9de8ba1df4ded4412b8256e03608c5
VictorCavalari <101271154+VictorCavalari@users.noreply.github.com>
2024-10-03 11:47:54 -0300

    criando arquivo common.js

---
+++ graficos/quantidadeUsuarios.js
@@ -0,0 +1,31 @@
+  async function quantidadeUsuarios() {
+    const url = 'https://raw.githubusercontent.com/guilhermeomrails/api/main/numero-usuarios.json'
+    const res = await fetch(url)
+    const dados = await res.json()
+    const nomeDasRedes = Object.keys(dados)
+    const quantidadeUsuarios = Object.values(dados)
+
+    const data = [
+        {
+          x: 'nomeDasRedes',
+          y: quantidadeDeUsuarios,
+          type: 'bar',
+          marker: {
+            color: getComputedStyle(document.body).getPropertyValue('--primary-color')
+          }
+        }
+      ]
+      
+
+      const grafico = document.createElement('div')
+grafico.className = 'grafico'
+document.getElementById('graficos-container').appendChild(grafico)
+Plotly.newPlot(grafico, data)
+  
+  }
+  
+  quantidadeUsuarios()
+
+  
+
+  
